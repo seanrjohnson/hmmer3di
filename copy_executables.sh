e91a315ee3ff0dc159335e55c24fd3349917519b
seanrjohnson <seanrjohnson@users.noreply.github.com>
2023-07-09 15:55:53 -0400

    copy_executables

--- copy_executables.sh
+++ copy_executables.sh
@@ -2,7 +2,7 @@ NEWDIR="hmmer3Di"
 prefix="3Di_"
 
 mkdir -p $NEWDIR
-for f in hmmalign hmmbuild hmmpress hmmsearch hmmscan
+for f in hmmalign hmmbuild hmmpress hmmsearch hmmscan phmmer
 do
     cp src/${f} ${NEWDIR}/${prefix}${f}
 done
